0b57afbf288b964d7b3f305801bf38870a9e1134
aurbanski <aurbanski27@gmail.com>
2018-03-15 13:37:31 -0600

    Added some of the rename

--- src/main/scala/jsy/student/Lab4.scala
+++ src/main/scala/jsy/student/Lab4.scala
@@ -232,15 +232,15 @@ object Lab4 extends jsy.util.JsyApplication with Lab4Like {
         case N(_) | B(_) | Undefined | S(_) => e
         case Print(e1) => Print(ren(env, e1))
 
-        case Unary(uop, e1) => ???
-        case Binary(bop, e1, e2) => ???
-        case If(e1, e2, e3) => ???
+        case Unary(uop, e1) => Unary(uop, ren(env, e1))
+        case Binary(bop, e1, e2) => Binary(bop, ren(env, e1), ren(env, e2))
+        case If(e1, e2, e3) => If(ren(env, e1), ren(env, e2), ren(env, e3))
 
         case Var(y) =>
-          ???
+          if(env.contains(y)) Var(env(y)) else Var(y)
         case Decl(mode, y, e1, e2) =>
           val yp = fresh(y)
-          ???
+          Decl(mode, yp, ren(env, e1), ren(env+(y->yp), e2))
 
         case Function(p, params, retty, e1) => {
           val (pp, envp): (Option[String], Map[String,String]) = p match {
